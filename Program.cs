3845364d8ee639bdb5fa8f32d8c714b10f86ea7c
Michael Hatoum <hatoumm89@gmail.com>
2022-11-19 00:13:01 -0500

    done

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
-﻿
+﻿using Newtonsoft.Json.Linq;
+using static System.Net.WebRequestMethods;
+using static System.Net.WebRequestMethods;
 
-https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+
+
+string apiKey = "6d2b93a6b18061428c87beb1cb18891b";
+var client = new HttpClient();
+
+Console.WriteLine("Enter zip code");
+var zipCode = Console.ReadLine();
+var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={apiKey}&units=imperial";
+
+var currentWeather = client.GetStringAsync(weatherURL).Result;
+var zipTemp = JObject.Parse(currentWeather)["main"]["temp"].ToString();
+Console.WriteLine($"Current temperature is: {zipTemp}°F.");
+
+
+
+
